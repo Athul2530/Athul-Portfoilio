6a818e46ee63453a47afedcebd1b57b6e37452b6
Athul2530 <162152975+Athul2530@users.noreply.github.com>
2024-07-19 23:36:59 +0530

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,4 +56,11 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
-
+function downloadFile() {
+    const link = document.createElement('a');
+    link.href = 'assets/img/Athul-resume.jpg';
+    link.download = 'Athul-resume.jpg';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
